1f09f9c0c1293163343bb4f95afcd82f7eaeb277
Andrey Panchenko <methos2013@gmail.com>
2019-07-07 12:48:58 +0300

    #Homework 7. Task 1

---
+++ FL11_HW7/homework/src/js/task1.js
@@ -0,0 +1,41 @@
+const minLengthEmail = 6;
+const minLengthPassword = 5;
+
+let email = prompt('Enter email address');
+if (email === null || email.trim().length === 0) {
+    console.log('Canceled.');
+} else if (email.trim().length < minLengthEmail) {
+    console.log("I don't know any emails having name length less than 6 symbols");
+} else if (
+    email.trim() === 'user@gmail.com' ||
+    email.trim() === 'admin@gmail.com'
+) {
+    let password = prompt('Enter password');
+    if (password === null || password.trim().length === 0) {
+        console.log('Canceled.');
+    } else if (
+        (email === 'user@gmail.com' && password === 'UserPass') ||
+        (email === 'admin@gmail.com' && password === 'AdminPass')
+    ) {
+        let willChanged = confirm('Do you want to change your password?');
+        if (willChanged) {
+            let oldPassword = prompt('Enter old password');
+            if (oldPassword === password) {
+                let newPassword = prompt('Enter new password');
+                if (newPassword.length < minLengthPassword) {
+                    console.log('It’s too short password. Sorry.');
+                } else {
+                    alert('You have successfully changed your password.');
+                }
+            } else {
+                console.log('You wrote the wrong password.');
+            }
+        } else {
+            console.log('You have failed the change.');
+        }
+    } else {
+        console.log('Wrong password');
+    }
+} else {
+    console.log('I don’t know you');
+}
